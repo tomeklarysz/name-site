021f2c5989af14ef1a8813f2f27e8664500f10cd
= <ttomek.larysz@gmail.com>
2025-03-26 22:37:46 +0100

    added text

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ ctx.moveTo(250,250);
 ctx.arc(250, 250, 215, 1.4*Math.PI, 1.6*Math.PI);
 ctx.fillStyle = 'red';
 ctx.fill();
+ctx.fillStyle = 'black';
+ctx.font = '15px Verdana';
+ctx.fillText('Płacisz 6 zł', 210, 100);
+ctx.fillText('za disney', 215, 120);
 
 ctx.beginPath();
 ctx.moveTo(250,250);
 ctx.arc(250, 250, 215, -0.4*Math.PI, 1.4*Math.PI);
 ctx.fillStyle = '#4461f3';
 ctx.fill();
+ctx.fillStyle = 'black';
+ctx.font = '18px Verdana';
+ctx.fillText('Niespodzianka', 188, 300);
 ctx.closePath();
 
 // pointer 
