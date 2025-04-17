1a397e988a6d41d9f2a181644d911f77bd13d2cd
= <ttomek.larysz@gmail.com>
2025-04-18 00:05:02 +0200

    draw more sectors with borders

--- index.js
+++ index.js
@@ -4,24 +4,36 @@ const ctx = canvas.getContext('2d');
 // wheel
 ctx.beginPath();
 ctx.moveTo(250,250);
-ctx.arc(250, 250, 215, 1.4*Math.PI, 1.6*Math.PI);
+ctx.arc(250, 250, 215, 1.8*Math.PI, 2*Math.PI);
+ctx.closePath();
 ctx.fillStyle = 'red';
 ctx.fill();
+ctx.strokeStyle = 'black';
+ctx.stroke();
 ctx.fillStyle = 'black';
 ctx.font = '15px Verdana';
-ctx.fillText('Płacisz 6 zł', 210, 100);
-ctx.fillText('za disney', 215, 120);
+// ctx.fillText('Płacisz 6 zł', 210, 100);
+// ctx.fillText('za disney', 215, 120);
 
-ctx.beginPath();
-ctx.moveTo(250,250);
-ctx.arc(250, 250, 215, -0.4*Math.PI, 1.4*Math.PI);
-ctx.fillStyle = '#4461f3';
-ctx.fill();
-ctx.fillStyle = 'black';
-ctx.font = '18px Verdana';
-ctx.fillText('Niespodzianka', 188, 300);
-ctx.closePath();
+let N = 9;
+let sectorsAngle = 0;
+for (let i=0; i<N; i++) {
+  ctx.beginPath();
+  ctx.moveTo(250,250);
+  ctx.arc(250, 250, 215, sectorsAngle*Math.PI, (sectorsAngle+0.2)*Math.PI);
+  ctx.closePath();
 
+  ctx.fillStyle = '#4461f3';
+  ctx.fill();
+
+  ctx.strokeStyle = 'black';
+  ctx.stroke();
+  ctx.fillStyle = 'black';
+  ctx.font = '18px Verdana';
+  // ctx.fillText('Niespodzianka', 188, 300);
+  ctx.closePath();
+  sectorsAngle += 0.2;
+}
 // pointer
 const pointer = document.getElementById('pointer');
 const ptr = pointer.getContext('2d');
