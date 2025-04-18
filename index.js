912cee5957456cb0e9307ced8c4870a25bdb54f3
= <ttomek.larysz@gmail.com>
2025-04-18 12:53:36 +0200

    range of angular velocity

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const canvas = document.getElementById('wheel');
 const ctx = canvas.getContext('2d');
+const pointer = document.getElementById('pointer');
+const ptr = pointer.getContext('2d');
+
+const containerSize = Math.min(window.innerWidth, window.innerHeight) * 0.9;
+canvas.style.width = containerSize + "px";
+canvas.style.height = containerSize + "px";
+pointer.style.width = containerSize + "px";
+pointer.style.height = containerSize + "px";
 
 const r = 215;
 const cx = canvas.width / 2;
@@ -8,22 +16,23 @@ const cy = canvas.height / 2;
 const N = 9;
 const angleStep = (2*Math.PI) / N;
 
+
 for (let i=0; i<N; i++) {
   
   const startAngle = i * angleStep;
   const endAngle = startAngle + angleStep;
   const middleAngle = (startAngle + endAngle) / 2;
-
+  
   ctx.beginPath();
   ctx.moveTo(cx, cy);
   ctx.arc(cx, cy, r, startAngle, endAngle);
   ctx.closePath();
   ctx.fillStyle = i == 8 ? 'red' :'#4461f3';
   ctx.fill();
-
+  
   ctx.strokeStyle = 'black';
   ctx.stroke();
-
+  
   const x = cx + 0.6*r*Math.cos(middleAngle);
   const y = cy + 0.6*r*Math.sin(middleAngle);
   
@@ -44,15 +53,13 @@ for (let i=0; i<N; i++) {
 ctx.canvas.style.transform = `rotate(${0.9}rad)`;
 
 // pointer
-const pointer = document.getElementById('pointer');
-const ptr = pointer.getContext('2d');
 ptr.beginPath();
 ptr.fillStyle = ' #2b3648';
 ptr.fillRect(450, 245, 30, 10);
 ptr.stroke();
 
-const MIN = 1.906
-const MAX = 1.913
+const MIN = 1.9101
+const MAX = 1.918
 const friction = 0.988;
 let angularVelocity = 0;
 let angle = 0;
@@ -74,7 +81,7 @@ const toDefaultRotation = () => {
 
 const rand = (m, M) => Math.random() * (M - m) + m;
 
-document.querySelector('button').addEventListener('click', () => {
+document.querySelector('#spin').addEventListener('click', () => {
   toDefaultRotation();
   spin();
 });
